cffc22f0d5f4d0032568b1204ba6feab0254679a
有種 <39034242+kenzok8@users.noreply.github.com>
2021-09-21 00:44:00 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -57,8 +57,8 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-udp2raw
 svn co https://github.com/immortalwrt/packages/trunk/net/udp2raw-tunnel
-svn co https://github.com/kenzok8/jell/trunk/luci-app-adguardhome
-svn co https://github.com/kenzok8/jell/trunk/adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-smartdns
 svn co https://github.com/immortalwrt/packages/trunk/net/smartdns
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gost
