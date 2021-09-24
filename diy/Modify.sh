f9585b6e5857c1821e646f7f56a34d26445fe1f5
有種 <39034242+kenzok8@users.noreply.github.com>
2021-09-24 23:34:37 +0800

    Update Modify.sh

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -3,6 +3,7 @@
 # Script for creating ACL file for each LuCI APP
 sed -i \
 -e "s/\(PKG_HASH\|PKG_MD5SUM\|PKG_MIRROR_HASH\):=.*/\1:=skip/" \
+-e 's?2. Clash For OpenWRT?3. Applications?' \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
 -e 's/ca-certificates/ca-bundle/' \
 */Makefile
