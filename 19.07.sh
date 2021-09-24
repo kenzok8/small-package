0db11d259768a9904897b0cb1c6e5790558aa7ad
有種 <39034242+kenzok8@users.noreply.github.com>
2021-09-24 19:44:36 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -57,7 +57,7 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-udp2raw
 svn co https://github.com/immortalwrt/packages/trunk/net/udp2raw-tunnel
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-smartdns
 svn co https://github.com/immortalwrt/packages/trunk/net/smartdns
