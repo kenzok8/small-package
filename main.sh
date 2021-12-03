2d666193c62b9bb6006d13d57a6d2cb88d64a4b3
有種 <39034242+kenzok8@users.noreply.github.com>
2021-12-03 14:19:21 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -67,6 +67,7 @@ git clone --depth 1 https://github.com/kenzok78/luci-theme-argonne
 git clone --depth 1 https://github.com/kiddin9/luci-app-wizard
 git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
 git clone --depth 1 https://github.com/kenzok78/minisign
+git clone --depth 1 https://github.com/kenzok78/luci-app-argonne-config
 
 svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-autoreboot
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
