00f47535d5abde937b73852ca47e785ac59b5a6e
kenzok8 <kenzok8@gmail.com>
2022-04-13 23:41:15 +0800

    int

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -9,22 +9,24 @@ sed -i \
 -e 's/ca-certificates/ca-bundle/' \
 */Makefile
 
-sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 sed -i 's/luci-lib-ipkg/luci-base/g' luci-app-store/Makefile
-
+rm -f miniupnpd/patches/106-spam-syslog*
 sed -i "/minisign:minisign/d" luci-app-dnscrypt-proxy2/Makefile
 sed -i 's/+libstdcpp/+libstdcpp +zlib/' ngrokc/Makefile
 sed -i 's/+dockerd/+dockerd +cgroupfs-mount/' luci-app-docker*/Makefile
 sed -i '$i /etc/init.d/dockerd restart &' luci-app-docker*/root/etc/uci-defaults/*
+sed -i '/\/etc\/config\/AdGuardHome/a /etc/AdGuardHome.yaml' luci-app-adguardhome/Makefile
 sed -i 's/+rclone\( \|$\)/+rclone +fuse-utils\1/g' luci-app-rclone/Makefile
 sed -i 's/+libcap /+libcap +libcap-bin /' luci-app-openclash/Makefile
 sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
+sed -i 's/\(+luci-compat\)/\1 +luci-theme-argonne/' luci-app-argonne-config/Makefile
 sed -i 's/+vsftpd$/+vsftpd-alt/' luci-app-vsftpd/Makefile
 sed -i 's/ +uhttpd-mod-ubus//' luci-app-packet-capture/Makefile
 sed -i '/boot()/,+2d' ddns-scripts/files/etc/init.d/ddns
 sed -i "/DISTRIB_DESCRIPTION/c\DISTRIB_DESCRIPTION=\"%D %C by kenzo'\"" base-files/files/etc/openwrt_release
 sed -i 's?admin/status/channel_analysis??' luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 sed -i "/--disable-https/d" netdata/Makefile
+sed -i -e '$a /etc/bench.log' \
 -e '/\/etc\/profile/d' \
 -e '/\/etc\/shinit/d' \
 base-files/files/lib/upgrade/keep.d/base-files-essential
@@ -35,8 +37,9 @@ sed -i '$a cgi-timeout = 300' uwsgi/files-luci-support/luci-webui.ini
 sed -i '$a cgi-timeout = 60' uwsgi/files-luci-support/luci-cgi_io.ini
 sed -i '/limit-as/c\limit-as = 5000' uwsgi/files-luci-support/luci-webui.ini
 sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/' uwsgi/files/uwsgi.init
+sed -i 's/	ip.neighbors/	luci.ip.neighbors/' luci-app-wifidog/luasrc/model/cbi/wifidog/wifidog_cfg.lua
 cp -rf base-files/files/sbin/sysupgrade my-default-settings/files/sbin/
-sed -i "s/CONFLICTS:=kmod-r8169//" r8168/Makefile
+
 
 bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
 bash diy/convert_translation.sh -a >/dev/null 2>&1
