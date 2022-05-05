8a9c15bd2b49d24a08092d479e0394c85b0a98c4
有種 <39034242+kenzok8@users.noreply.github.com>
2022-05-05 11:25:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -92,7 +92,7 @@ git clone --depth 1 https://github.com/kenzok78/luci-theme-argonne
 git clone --depth 1 https://github.com/kenzok78/luci-app-argonne-config
 git clone --depth 1 https://github.com/thinktip/luci-theme-neobird
 git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns
-git clone --depth 1  https://github.com/ophub/luci-app-amlogic amlogic && mv -n amlogic/luci-app-amlogic ./;rm -rf amlogic
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic amlogic && mv -n amlogic/luci-app-amlogic ./;rm -rf amlogic
 git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant
