1b586971bae154c3bd724e9a2c74e5625e2e1be7
有種 <39034242+kenzok8@users.noreply.github.com>
2022-11-02 23:21:47 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -100,6 +100,7 @@ git clone --depth 1 https://github.com/linkease/istore && mv -n istore/luci/* ./
 git clone --depth 1 https://github.com/linkease/openwrt-app-actions
 git clone --depth 1 https://github.com/ZeaKyX/luci-app-speedtest-web
 git clone --depth 1 https://github.com/ZeaKyX/speedtest-web
+git clone --depth 1 https://github.com/Huangjoe123/luci-app-eqos
 
 svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-ipkg
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant
