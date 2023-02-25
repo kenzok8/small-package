d327d3722ed0c62fc2c38d7a7acb07f28f5a35aa
kenzok8 <kenzok8@gmail.com>
2023-02-25 18:04:19 +0800

    int

--- .github/diy/Modify.sh
+++ .github/diy/Modify.sh
@@ -5,5 +5,6 @@ rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
 rm -rf create_acl_for_luci.warn
+cp -f .github/diy/packages/files/usr/lib/lua/luci/view/admin_status/index/links.htm my-default-settings/files/usr/lib/lua/luci/view/admin_status/index/links.htm
 
 exit 0
