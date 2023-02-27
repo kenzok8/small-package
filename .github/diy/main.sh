eaf94b09eee0dfc967322e2252cb48c9987ddc0a
有種 <39034242+kenzok8@users.noreply.github.com>
2023-02-27 13:10:39 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -99,7 +99,7 @@ git clone --depth 1 https://github.com/linkease/nas-packages && mv -n nas-packag
 git clone --depth 1 https://github.com/linkease/nas-packages-luci && mv -n nas-packages-luci/luci/* ./; rm -rf nas-packages-luci
 git clone --depth 1 https://github.com/linkease/istore && mv -n istore/luci/* ./; rm -rf istore
 git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd
-git_clone https://github.com/linkease/openwrt-app-actions && mv -n openwrt-app-actions/applications ./;rm -rf openwrt-app-actions
+git clone --depth 1 https://github.com/linkease/openwrt-app-actions && mv -n openwrt-app-actions/applications/* ./;rm -rf openwrt-app-actions
 git clone --depth 1 https://github.com/ZeaKyX/luci-app-speedtest-web
 git clone --depth 1 https://github.com/ZeaKyX/speedtest-web
 git clone --depth 1 https://github.com/Huangjoe123/luci-app-eqos
