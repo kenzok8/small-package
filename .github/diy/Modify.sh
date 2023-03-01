21ac70287003d2c98e61c8d0ddf1d4bfdf70242c
有種 <39034242+kenzok8@users.noreply.github.com>
2023-03-01 19:19:43 +0800

    Update Modify.sh

--- .github/diy/Modify.sh
+++ .github/diy/Modify.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # --------------------------------------------------------
 # Script for creating ACL file for each LuCI APP
-rm -rf ./*/.git & rm -f ./*/.gitattributes
-rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
-rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
-rm -rf create_acl_for_luci.warn
-
+rm -rf ./*/.git ./*/.gitattributes ./*/.svn ./*/.github ./*/.gitignore create_acl_for_luci.err create_acl_for_luci.ok create_acl_for_luci.warn
 exit 0
