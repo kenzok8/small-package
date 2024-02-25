38f8be0e913ab5527c919389c3b84ca6c584e9d9
有種 <39034242+kenzok8@users.noreply.github.com>
2024-02-25 20:19:55 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -182,6 +182,7 @@ git_sparse_clone master "https://github.com/immortalwrt/immortalwrt" "immortal" 
 package/network/utils/fullconenat package/network/utils/fullconenat-nft \
 package/utils/mhz package/libs/libnftnl package/firmware/wireless-regdb
 
+git_sparse_clone master "https://github.com/sbwml/openwrt_pkgs" "openwrt_pkgs" luci-app-gowebdav luci-app-ota luci-app-socat \
 git_sparse_clone openwrt-23.05 "https://github.com/openwrt/luci" "opluci" applications/luci-app-watchcat \
 
 #mv -n openwrt-passwall/* ./ ; rm -Rf openwrt-passwall
