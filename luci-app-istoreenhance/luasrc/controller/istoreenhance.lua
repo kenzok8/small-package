de0d23da18e0e726906966e5f52bf6a5bf44dca0
kenzok8 <kenzok8@users.noreply.github.com>
2024-06-24 20:34:31 +0800

    update 2024-06-24 20:34:31

---
+++ luci-app-istoreenhance/luasrc/controller/istoreenhance.lua
@@ -0,0 +1,26 @@
+module("luci.controller.istoreenhance", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/istoreenhance") then
+		return
+	end
+
+	entry({"admin", "services", "istoreenhance"}, cbi("istoreenhance"), _("iStoreEnhance"), 20).dependent = true
+
+	entry({"admin", "services", "istoreenhance_status"}, call("istoreenhance_status"))
+end
+
+function istoreenhance_status()
+	local sys  = require "luci.sys"
+	local uci  = require "luci.model.uci".cursor()
+	local port = tonumber(uci:get_first("istoreenhance", "istoreenhance", "port"))
+
+	local status = {
+		running = (sys.call("pidof iStoreEnhance >/dev/null") == 0),
+		port = (port or 8897)
+	}
+
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(status)
+end
+
