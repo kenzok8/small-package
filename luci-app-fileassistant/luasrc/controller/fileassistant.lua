c38bd86ebf759cb5ae833a84e1f2820dcbf278fc
kenzok8 <kenzok8@users.noreply.github.com>
2024-07-07 09:20:21 +0800

    update 2024-07-07 09:20:21

--- luci-app-fileassistant/luasrc/controller/fileassistant.lua
+++ luci-app-fileassistant/luasrc/controller/fileassistant.lua
@@ -3,7 +3,7 @@ module("luci.controller.fileassistant", package.seeall)
 function index()
 
     local page
-    page = entry({"admin", "system", "fileassistant"}, template("fileassistant"), _("文件助手"), 1)
+    page = entry({"admin", "system", "fileassistant"}, template("fileassistant"), _("文件管理"), 50)
     page.i18n = "base"
     page.dependent = true
     page.acl_depends = { "luci-app-fileassistant" }
