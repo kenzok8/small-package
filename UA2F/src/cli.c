feffb2f51703784c82ea3743626d9e6081724266
kenzok8 <kenzok8@users.noreply.github.com>
2024-08-07 20:34:42 +0800

    update 2024-08-07 20:34:42

--- UA2F/src/cli.c
+++ UA2F/src/cli.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/syslog.h>
 
 #include "cli.h"
+
 #include "config.h"
+
+void startup_message() {
+    syslog(LOG_INFO, "UA2F version: %s", UA2F_VERSION);
+    syslog(LOG_INFO, "Git commit: %s", UA2F_GIT_COMMIT);
+    syslog(LOG_INFO, "Git branch: %s", UA2F_GIT_BRANCH);
+    syslog(LOG_INFO, "Git tag: %s", UA2F_GIT_TAG);
+}
 
 // handle print --version and --help
 void try_print_info(const int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
+        startup_message();
         return;
     }
 
