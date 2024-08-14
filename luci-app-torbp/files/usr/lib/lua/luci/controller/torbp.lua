5ac54b621060509c73f2dba0216d05c1667831a5
kenzok8 <kenzok8@users.noreply.github.com>
2024-08-14 12:22:20 +0800

    update 2024-08-14 12:22:20

--- luci-app-torbp/files/usr/lib/lua/luci/controller/torbp.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.torbp", package.seeall)
-function index()
-	if not nixio.fs.access("/etc/config/torbp") then
-		return
-	end
-	local page
-	page = entry({"admin", "services", "torbp"}, cbi("torbp"), _("Tor bridges proxy"))
-	page.dependent = true
-end
