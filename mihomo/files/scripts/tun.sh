a95cce2661d5410b7bc96454f645922f7abf0131
kenzok8 <kenzok8@users.noreply.github.com>
2024-09-06 00:23:45 +0800

    update 2024-09-06 00:23:45

--- mihomo/files/scripts/tun.sh
+++ mihomo/files/scripts/tun.sh
@@ -3,22 +3,15 @@
 . "$IPKG_INSTROOT/lib/functions.sh"
 . "$IPKG_INSTROOT/etc/mihomo/scripts/constants.sh"
 
-load_config() {
-	config_load mihomo
-	config_get enabled "config" "enabled" 0
-	config_get transparent_proxy_mode "proxy" "transparent_proxy_mode"
-}
+config_load mihomo
+config_get enabled "config" "enabled" 0
+config_get tcp_transparent_proxy_mode "proxy" "tcp_transparent_proxy_mode"
+config_get udp_transparent_proxy_mode "proxy" "udp_transparent_proxy_mode"
 
-accept_tun() {
+if [ "$enabled" == 1 ] && [[ "$tcp_transparent_proxy_mode" == "tun" || "$udp_transparent_proxy_mode" == "tun" ]]; then
 	nft insert rule inet fw4 input iifname "$TUN_DEVICE" counter accept
 	nft insert rule inet fw4 forward oifname "$TUN_DEVICE" counter accept
 	nft insert rule inet fw4 forward iifname "$TUN_DEVICE" counter accept
-}
-
-load_config
-
-if [[ "$enabled" == 0 || "$transparent_proxy_mode" != "tun" ]]; then
-	return
 fi
 
-accept_tun
+exit 0
