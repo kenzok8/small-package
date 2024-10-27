f495ea5d4bcbb9d26079872f558eb1788c5acad6
kenzok8 <kenzok8@users.noreply.github.com>
2024-10-28 00:23:52 +0800

    update 2024-10-28 00:23:52

--- luci-app-nekobox/htdocs/nekobox/update_config.php
+++ luci-app-nekobox/htdocs/nekobox/update_config.php
@@ -11,22 +11,32 @@ function logMessage($message) {
 }
 
 $urls = [
-    "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/mihomo.yaml" => "/etc/neko/config/mihomo.yaml",
-    "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/Puernya.json" => "/etc/neko/config/Puernya.json"
+    "https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/mihomo.yaml" => "/etc/neko/config/mihomo.yaml",
+    "https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/Puernya.json" => "/etc/neko/config/Puernya.json"
 ];
+
+function downloadFile($url, $path, $retries = 3) {
+    for ($i = 0; $i < $retries; $i++) {
+        $command = "curl -L --fail -o '$path' '$url'";
+        exec($command, $output, $return_var);
+
+        if ($return_var === 0) {
+            logMessage(basename($path) . " 文件已成功更新！");
+            return true;
+        } else {
+            logMessage("下载失败：$path，重试中（" . ($i + 1) . "/$retries）...");
+            sleep(2);  
+        }
+    }
+    logMessage("下载失败：$path，已超过最大重试次数！");
+    return false;
+}
 
 foreach ($urls as $download_url => $destination_path) {
     if (!is_dir(dirname($destination_path))) {
         mkdir(dirname($destination_path), 0755, true);
     }
-
-    exec("wget -O '$destination_path' '$download_url'", $output, $return_var);
-    if ($return_var !== 0) {
-        logMessage("下载失败：$destination_path");
-        die("下载失败：$destination_path");
-    }
-
-    logMessage(basename($destination_path) . " 文件已成功更新！");
+    downloadFile($download_url, $destination_path);
 }
 
 echo implode("\n", $logMessages);
