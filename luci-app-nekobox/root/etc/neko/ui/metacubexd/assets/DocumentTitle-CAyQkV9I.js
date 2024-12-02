3728c6a6778a6ab9ef789432ade3137032ceff5b
kenzok8 <kenzok8@users.noreply.github.com>
2024-12-02 16:29:44 +0800

    update 2024-12-02 16:29:44

--- luci-app-nekobox/root/etc/neko/ui/metacubexd/assets/DocumentTitle-CAyQkV9I.js
+++
@@ -1,1 +0,0 @@
-import{d as t,bv as r}from"./index-CvtbhWKk.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
--- luci-app-nekobox/root/etc/neko/ui/metacubexd/assets/DocumentTitle-D8u3UFYZ.js
+++
@@ -1,1 +0,0 @@
-import{d as t,bw as r}from"./index-CWCcHU9x.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
