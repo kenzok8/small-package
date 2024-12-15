35a7d2bf4aad4e40bd3328dd8f8230b8557e93d7
kenzok8 <kenzok8@users.noreply.github.com>
2024-12-16 00:24:10 +0800

    update 2024-12-16 00:24:10

---
+++ luci-app-ubuntu2/luasrc/model/cbi/ubuntu2.lua
@@ -0,0 +1,63 @@
+--[[
+LuCI - Lua Configuration Interface
+]]--
+
+local taskd = require "luci.model.tasks"
+local docker = require "luci.docker"
+local ubuntu2_model = require "luci.model.ubuntu2"
+local m, s, o
+
+m = taskd.docker_map("ubuntu2", "ubuntu2", "/usr/libexec/istorec/ubuntu2.sh",
+	translate("Ubuntu2"),
+	translate("Ubuntu2 is a high-Performance ubuntu with web remote desktop. [username: abc, password is empty]")
+		.. translate("Official website:") .. ' <a href=\"https://www.kasmweb.com\" target=\"_blank\">https://www.kasmweb.com</a>')
+
+local dk = docker.new({socket_path="/var/run/docker.sock"})
+local dockerd_running = dk:_ping().code == 200
+local docker_info = dockerd_running and dk:info().body or {}
+local docker_aspace = 0
+if docker_info.DockerRootDir then
+        local statvfs = nixio.fs.statvfs(docker_info.DockerRootDir)
+        docker_aspace = statvfs and (statvfs.bavail * statvfs.bsize) or 0
+end
+
+s = m:section(SimpleSection, translate("Service Status"), translate("Ubuntu2 status:"))
+s:append(Template("ubuntu2/status"))
+
+s = m:section(TypedSection, "main", translate("Setup"), translate("The following parameters will only take effect during installation or upgrade:"))
+s.addremove=false
+s.anonymous=true
+
+o = s:option(Value, "https_port", translate("HTTPS Port").."<b>*</b>")
+o.default = "3001"
+o.datatype = "port"
+
+o = s:option(Value, "image_name", translate("Image").."<b>*</b>")
+o.rmempty = false
+o.datatype = "string"
+o:value("linkease/desktop-ubuntu2-standard-arm64:latest", "Standard-ARM64")
+o:value("linkease/desktop-ubuntu2-standard-amd64:latest", "Standard-AMD64")
+if "x86_64" == docker_info.Architecture then
+  o.default = "linkease/desktop-ubuntu2-standard-amd64:latest"
+else
+  o.default = "linkease/desktop-ubuntu2-standard-arm64:latest"
+end
+
+o = s:option(Value, "password", "PASSWORD")
+o.password = true
+o.datatype = "string"
+
+local blocks = ubuntu2_model.blocks()
+local home = ubuntu2_model.home()
+
+o = s:option(Value, "config_path", translate("Config path").."<b>*</b>")
+o.rmempty = false
+o.datatype = "string"
+
+local paths, default_path = ubuntu2_model.find_paths(blocks, home, "Configs")
+for _, val in pairs(paths) do
+  o:value(val, val)
+end
+o.default = default_path
+
+return m
---
+++ luci-app-ubuntu2/root/usr/libexec/istorec/ubuntu2.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+# Author Xiaobao(xiaobao@linkease.com)
+
+ACTION=${1}
+shift 1
+
+do_install() {
+  local https_port=`uci get ubuntu2.@main[0].https_port 2>/dev/null`
+  local image_name=`uci get ubuntu2.@main[0].image_name 2>/dev/null`
+  local config=`uci get ubuntu2.@main[0].config_path 2>/dev/null`
+  if [ -z ${IMAGE_NAME} ]; then
+    local arch=`uname -m`
+    if [ "$arch" = "x86_64" ]; then
+      IMAGE_NAME=linkease/desktop-ubuntu2-standard-amd64:latest
+    else
+      IMAGE_NAME=linkease/desktop-ubuntu2-standard-arm64:latest
+    fi
+  fi
+
+  echo "docker pull ${image_name}"
+  docker pull ${image_name}
+  docker rm -f ubuntu2
+
+  if [ -z "$config" ]; then
+      echo "config path is empty!"
+      exit 1
+  fi
+
+  # not conflict with jellyfin
+  [ -z "$https_port" ] && https_port=3001
+
+  local cmd="docker run --restart=unless-stopped -d --user 0:0 -e START_DOCKER=false -v \"$config:/config\" \
+    --privileged --device /dev/fuse --security-opt apparmor=unconfined --shm-size=512m \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --dns=172.17.0.1 \
+    -p $https_port:3001 "
+
+  if [ -d /dev/dri ]; then
+    cmd="$cmd\
+    --device /dev/dri:/dev/dri "
+  fi
+
+  local tz="`uci get system.@system[0].zonename | sed 's/ /_/g'`"
+  [ -z "$tz" ] || cmd="$cmd -e TZ=$tz"
+
+  cmd="$cmd -v /mnt:/mnt"
+  mountpoint -q /mnt && cmd="$cmd:rslave"
+  cmd="$cmd --name ubuntu2 \"$image_name\""
+
+  echo "$cmd"
+  eval "$cmd"
+}
+
+usage() {
+  echo "usage: $0 sub-command"
+  echo "where sub-command is one of:"
+  echo "      install                Install the ubuntu2"
+  echo "      upgrade                Upgrade the ubuntu2"
+  echo "      rm/start/stop/restart  Remove/Start/Stop/Restart the ubuntu2"
+  echo "      status                 Ubuntu2 status"
+  echo "      port                   Ubuntu2 port"
+}
+
+case ${ACTION} in
+  "install")
+    do_install
+  ;;
+  "upgrade")
+    do_install
+  ;;
+  "rm")
+    docker rm -f ubuntu2
+  ;;
+  "start" | "stop" | "restart")
+    docker ${ACTION} ubuntu2
+  ;;
+  "status")
+    docker ps --all -f 'name=^/ubuntu2$' --format '{{.State}}'
+  ;;
+  "port")
+    docker ps --all -f 'name=^/ubuntu2$' --format '{{.Ports}}' | grep -om1 '0.0.0.0:[0-9]*->3001/tcp' | sed 's/0.0.0.0:\([0-9]*\)->.*/\1/'
+  ;;
+  *)
+    usage
+    exit 1
+  ;;
+esac
