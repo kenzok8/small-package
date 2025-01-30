38173d7ab30d545672395418f33e17259615fbf2
kenzok8 <kenzok8@users.noreply.github.com>
2025-01-30 09:22:45 +0800

    update 2025-01-30 09:22:45

--- luci-app-nekobox/htdocs/nekobox/theme.php
+++ luci-app-nekobox/htdocs/nekobox/theme.php
@@ -28,9 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $checkColor = $_POST['checkColor'] ?? '#0eaf3e';
     $labelColor = $_POST['labelColor'] ?? '#0eaf3e';
     $lineColor = $_POST['lineColor'] ?? '#f515f9';
+    $disabledColor = $_POST['disabledColor'] ?? '#23407e';
+    $radiusColor = $_POST['radiusColor'] ?? '#14b863';
     $themeName = isset($_POST['themeName']) ? $_POST['themeName'] : 'transparent';   
 
-    $themeName = preg_replace('/[\p{Han}]/u', '', $themeName);
+    $themeName = preg_replace('/[^a-zA-Z0-9_\-一-龯]/u', '', $themeName);
     if (empty($themeName)) {
         $themeName = 'transparent'; 
     }
@@ -73,6 +75,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       --bs-check-bg: $checkColor;
       --bs-label-bg: $labelColor;
       --bs-line-bg: $lineColor;
+      --bs-disabled-bg: $disabledColor;
+      --bs-radius-bg: $radiusColor;
 
       --bs-primary-border-subtle: $primaryBorderSubtle; 
       --bs-tertiary: $tertiaryColor; 
@@ -126,6 +130,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     input::placeholder {
         color: var(--placeholder-color) !important;
         font-weight: bold; 
+    }
+
+    input.form-control:disabled {
+      background-color: var(--bs-disabled-bg) !important; 
+
     }
 
     #lineColumnDisplay, #charCountDisplay {
@@ -185,14 +194,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     .form-select {
         background-color: var(--bs-form-select);
-        color: var(--bs-controlr-bg) !important;            
+        color: var(--bs-radius-bg) !important;            
         border: 1px solid gray;    
         border-radius: 5px;       
     }
 
     .form-select option {
         background-color: var(--bs-form-select);    
-        color: var(--bs-controlr-bg) !important;           
+        color: var(--bs-radius-bg) !important;           
     }
 
     .form-control {
