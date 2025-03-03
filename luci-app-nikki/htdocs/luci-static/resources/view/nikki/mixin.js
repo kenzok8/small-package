c0bcb9092398058b7f5488767ecbbe035cd5a1c9
kenzok8 <kenzok8@users.noreply.github.com>
2025-03-04 00:24:44 +0800

    update 2025-03-04 00:24:44

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/mixin.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/mixin.js
@@ -18,103 +18,104 @@ return view.extend({
 
         m = new form.Map('nikki');
 
-        s = m.section(form.NamedSection, 'config', 'config', _('Mixin Config'));
-
-        o = s.option(form.Flag, 'mixin', _('Enable'));
-        o.rmempty = false;
-
         s = m.section(form.NamedSection, 'mixin', 'mixin', _('Mixin Option'));
 
         s.tab('general', _('General Config'));
 
-        o = s.taboption('general', form.ListValue, 'log_level', '*' + ' ' + _('Log Level'));
+        o = s.taboption('general', form.ListValue, 'log_level', _('Log Level'));
+        o.optional = true;
         o.value('silent');
         o.value('error');
         o.value('warning');
         o.value('info');
         o.value('debug');
 
-        o = s.taboption('general', form.ListValue, 'mode', '*' + ' ' + _('Mode'));
+        o = s.taboption('general', form.ListValue, 'mode', _('Mode'));
+        o.optional = true;
         o.value('global', _('Global Mode'));
         o.value('rule', _('Rule Mode'));
         o.value('direct', _('Direct Mode'));
 
-        o = s.taboption('general', form.ListValue, 'match_process', '*' + ' ' + _('Match Process'));
-        o.value('strict', _('Auto'));
-        o.value('always', _('Enable'));
-        o.value('off', _('Disable'));
+        o = s.taboption('general', form.ListValue, 'match_process', _('Match Process'));
+        o.optional = true;
+        o.value('off');
+        o.value('strict');
+        o.value('always');
 
-        o = s.taboption('general', widgets.NetworkSelect, 'outbound_interface', '*' + ' ' + _('Outbound Interface'));
+        o = s.taboption('general', widgets.NetworkSelect, 'outbound_interface', _('Outbound Interface'));
         o.optional = true;
 
-        o = s.taboption('general', form.Flag, 'ipv6', '*' + ' ' + _('IPv6'));
-        o.rmempty = false;
+        o = s.taboption('general', form.ListValue, 'ipv6', _('IPv6'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('general', form.Flag, 'unify_delay', _('Unify Delay'));
-        o.rmempty = false;
+        o = s.taboption('general', form.ListValue, 'unify_delay', _('Unify Delay'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('general', form.Flag, 'tcp_concurrent', _('TCP Concurrent'));
-        o.rmempty = false;
+        o = s.taboption('general', form.ListValue, 'tcp_concurrent', _('TCP Concurrent'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
         o = s.taboption('general', form.Value, 'tcp_keep_alive_idle', _('TCP Keep Alive Idle'));
         o.datatype = 'uinteger';
-        o.placeholder = '600';
 
         o = s.taboption('general', form.Value, 'tcp_keep_alive_interval', _('TCP Keep Alive Interval'));
         o.datatype = 'uinteger';
-        o.placeholder = '15';
 
         s.tab('external_control', _('External Control Config'));
 
         o = s.taboption('external_control', form.Value, 'ui_path', '*' + ' ' + _('UI Path'));
         o.rmempty = false;
 
-        o = s.taboption('external_control', form.Value, 'ui_name', '*' + ' ' + _('UI Name'));
+        o = s.taboption('external_control', form.Value, 'ui_name', _('UI Name'));
 
-        o = s.taboption('external_control', form.Value, 'ui_url', '*' + ' ' + _('UI Url'));
-        o.rmempty = false;
+        o = s.taboption('external_control', form.Value, 'ui_url', _('UI Url'));
         o.value('https://github.com/Zephyruso/zashboard/archive/refs/heads/gh-pages.zip', 'Zashboard');
         o.value('https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip', 'MetaCubeXD');
         o.value('https://github.com/MetaCubeX/Yacd-meta/archive/refs/heads/gh-pages.zip', 'YACD');
         o.value('https://github.com/MetaCubeX/Razord-meta/archive/refs/heads/gh-pages.zip', 'Razord');
 
-        o = s.taboption('external_control', form.Value, 'api_port', '*' + ' ' + _('API Port'));
-        o.datatype = 'port';
-        o.placeholder = '9090';
+        o = s.taboption('external_control', form.Value, 'api_listen', '*' + ' ' + _('API Listen'));
+        o.datatype = 'ipaddrport(1)';
+        o.rmempty = false;
 
-        o = s.taboption('external_control', form.Value, 'api_secret', '*' + ' ' + _('API Secret'));
+        o = s.taboption('external_control', form.Value, 'api_secret', _('API Secret'));
         o.password = true;
-        o.rmempty = false;
 
-        o = s.taboption('external_control', form.Flag, 'selection_cache', '*' + ' ' + _('Save Proxy Selection'));
-        o.rmempty = false;
+        o = s.taboption('external_control', form.ListValue, 'selection_cache', _('Save Proxy Selection'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
         s.tab('inbound', _('Inbound Config'));
 
-        o = s.taboption('inbound', form.Flag, 'allow_lan', '*' + ' ' + _('Allow Lan'));
-        o.rmempty = false;
+        o = s.taboption('inbound', form.ListValue, 'allow_lan', _('Allow Lan'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('inbound', form.Value, 'http_port', '*' + ' ' + _('HTTP Port'));
+        o = s.taboption('inbound', form.Value, 'http_port', _('HTTP Port'));
         o.datatype = 'port';
-        o.placeholder = '8080';
 
-        o = s.taboption('inbound', form.Value, 'socks_port', '*' + ' ' + _('SOCKS Port'));
+        o = s.taboption('inbound', form.Value, 'socks_port', _('SOCKS Port'));
         o.datatype = 'port';
-        o.placeholder = '1080';
 
-        o = s.taboption('inbound', form.Value, 'mixed_port', '*' + ' ' + _('Mixed Port'));
+        o = s.taboption('inbound', form.Value, 'mixed_port', _('Mixed Port'));
         o.datatype = 'port';
-        o.placeholder = '7890';
 
         o = s.taboption('inbound', form.Value, 'redir_port', '*' + ' ' + _('Redirect Port'));
         o.datatype = 'port';
-        o.placeholder = '7891';
+        o.rmempty = false;
 
         o = s.taboption('inbound', form.Value, 'tproxy_port', '*' + ' ' + _('TPROXY Port'));
         o.datatype = 'port';
-        o.placeholder = '7892';
+        o.rmempty = false;
 
-        o = s.taboption('inbound', form.Flag, 'authentication', '*' + ' ' + _('Overwrite Authentication'));
+        o = s.taboption('inbound', form.Flag, 'authentication', _('Overwrite Authentication'));
         o.rmempty = false;
 
         o = s.taboption('inbound', form.SectionValue, '_authentications', form.TableSection, 'authentication', _('Edit Authentications'));
@@ -137,90 +138,97 @@ return view.extend({
 
         s.tab('tun', _('TUN Config'));
 
-        o = s.taboption('tun', form.Value, 'tun_device', '*' + ' ' + _('Device'));
+        o = s.taboption('tun', form.Value, 'tun_device', '*' + ' ' + _('Device Name'));
         o.rmempty = false;
 
-        o = s.taboption('tun', form.ListValue, 'tun_stack', '*' + ' ' + _('Stack'));
+        o = s.taboption('tun', form.ListValue, 'tun_stack', _('Stack'));
+        o.optional = true;
         o.value('system', 'System');
         o.value('gvisor', 'gVisor');
         o.value('mixed', 'Mixed');
 
-        o = s.taboption('tun', form.Value, 'tun_mtu', '*' + ' ' + _('MTU'));
+        o = s.taboption('tun', form.Value, 'tun_mtu', _('MTU'));
         o.datatype = 'uinteger';
-        o.placeholder = '9000';
 
-        o = s.taboption('tun', form.Flag, 'tun_gso', '*' + ' ' + _('GSO'));
-        o.rmempty = false;
+        o = s.taboption('tun', form.ListValue, 'tun_gso', _('GSO'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('tun', form.Value, 'tun_gso_max_size', '*' + ' ' + _('GSO Max Size'));
+        o = s.taboption('tun', form.Value, 'tun_gso_max_size', _('GSO Max Size'));
         o.datatype = 'uinteger';
-        o.placeholder = '65536';
+
+        o = s.taboption('tun', form.ListValue, 'tun_endpoint_independent_nat', _('Endpoint Independent NAT'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
+
+        o = s.taboption('tun', form.Flag, 'tun_dns_hijack', _('Overwrite DNS Hijack'));
+        o.rmempty = false;
+
+        o = s.taboption('tun', form.DynamicList, 'tun_dns_hijacks', _('Edit DNS Hijacks'));
         o.retain = true;
-        o.depends('tun_gso', '1');
-
-        o = s.taboption('tun', form.Flag, 'tun_endpoint_independent_nat', '*' + ' ' + _('Endpoint Independent NAT'));
-        o.rmempty = false;
-
-        o = s.taboption('tun', form.Flag, 'tun_dns_hijack', '*' + ' ' + _('Overwrite DNS Hijack'));
-        o.rmempty = false;
-
-        o = s.taboption('tun', form.DynamicList, 'tun_dns_hijacks', '*' + ' ' + _('Edit DNS Hijacks'));
-        o.retain = true;
-        o.rmempty = false;
         o.depends('tun_dns_hijack', '1');
         o.value('tcp://any:53');
         o.value('udp://any:53');
 
         s.tab('dns', _('DNS Config'));
 
-        o = s.taboption('dns', form.Value, 'dns_port', '*' + ' ' + _('DNS Port'));
-        o.datatype = 'port';
-        o.placeholder = '1053';
-
-        o = s.taboption('dns', form.Flag, 'dns_ipv6', '*' + ' ' + _('IPv6'));
+        o = s.taboption('dns', form.Value, 'dns_listen', '*' + ' ' + _('DNS Listen'));
+        o.datatype = 'ipaddrport(1)';
         o.rmempty = false;
 
+        o = s.taboption('dns', form.ListValue, 'dns_ipv6', _('IPv6'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
+
         o = s.taboption('dns', form.ListValue, 'dns_mode', '*' + ' ' + _('DNS Mode'));
-        o.value('fake-ip', 'Fake-IP');
         o.value('redir-host', 'Redir-Host');
+        o.value('fake-ip', 'Fake-IP');
 
         o = s.taboption('dns', form.Value, 'fake_ip_range', '*' + ' ' + _('Fake-IP Range'));
         o.datatype = 'cidr4';
-        o.placeholder = '198.18.0.1/16';
         o.retain = true;
+        o.rmempty = false;
         o.depends('dns_mode', 'fake-ip');
 
         o = s.taboption('dns', form.Flag, 'fake_ip_filter', _('Overwrite Fake-IP Filter'));
-        o.retain = true;
         o.rmempty = false;
-        o.depends('dns_mode', 'fake-ip');
 
         o = s.taboption('dns', form.DynamicList, 'fake_ip_filters', _('Edit Fake-IP Filters'));
         o.retain = true;
-        o.depends({ 'dns_mode': 'fake-ip', 'fake_ip_filter': '1' });
+        o.depends('fake_ip_filter', '1');
 
         o = s.taboption('dns', form.ListValue, 'fake_ip_filter_mode', _('Fake-IP Filter Mode'));
-        o.retain = true;
+        o.optional = true;
         o.value('blacklist', _('Block Mode'));
         o.value('whitelist', _('Allow Mode'));
-        o.depends({ 'dns_mode': 'fake-ip', 'fake_ip_filter': '1' });
 
-        o = s.taboption('dns', form.Flag, 'fake_ip_cache', '*' + ' ' + _('Fake-IP Cache'));
-        o.retain = true;
-        o.rmempty = false;
-        o.depends('dns_mode', 'fake-ip');
+        o = s.taboption('dns', form.ListValue, 'fake_ip_cache', _('Fake-IP Cache'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('dns', form.Flag, 'dns_respect_rules', _('Respect Rules'));
-        o.rmempty = false;
+        o = s.taboption('dns', form.ListValue, 'dns_respect_rules', _('Respect Rules'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('dns', form.Flag, 'dns_doh_prefer_http3', _('DoH Prefer HTTP/3'));
-        o.rmempty = false;
+        o = s.taboption('dns', form.ListValue, 'dns_doh_prefer_http3', _('DoH Prefer HTTP/3'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('dns', form.Flag, 'dns_system_hosts', _('Use System Hosts'));
-        o.rmempty = false;
+        o = s.taboption('dns', form.ListValue, 'dns_system_hosts', _('Use System Hosts'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('dns', form.Flag, 'dns_hosts', _('Use Hosts'));
-        o.rmempty = false;
+        o = s.taboption('dns', form.ListValue, 'dns_hosts', _('Use Hosts'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
         o = s.taboption('dns', form.Flag, 'hosts', _('Overwrite Hosts'));
         o.rmempty = false;
@@ -285,17 +293,20 @@ return view.extend({
 
         s.tab('sniffer', _('Sniffer Config'));
 
-        o = s.taboption('sniffer', form.Flag, 'sniffer', _('Enable'));
-        o.rmempty = false;
+        o = s.taboption('sniffer', form.ListValue, 'sniffer', _('Enable'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('sniffer', form.Flag, 'sniffer_sniff_dns_mapping', _('Sniff Redir-Host'));
-        o.rmempty = false;
+        o = s.taboption('sniffer', form.ListValue, 'sniffer_sniff_dns_mapping', _('Sniff Redir-Host'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
-        o = s.taboption('sniffer', form.Flag, 'sniffer_sniff_pure_ip', _('Sniff Pure IP'));
-        o.rmempty = false;
-
-        o = s.taboption('sniffer', form.Flag, 'sniffer_overwrite_destination', _('Overwrite Destination'));
-        o.rmempty = false;
+        o = s.taboption('sniffer', form.ListValue, 'sniffer_sniff_pure_ip', _('Sniff Pure IP'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
         o = s.taboption('sniffer', form.Flag, 'sniffer_force_domain_name', _('Overwrite Force Sniff Domain Name'));
         o.rmempty = false;
@@ -336,7 +347,7 @@ return view.extend({
 
         s.tab('rule', _('Rule Config'));
 
-        o = s.taboption('rule', form.Flag, 'rule_provider', '*' + ' ' + _('Append Rule Provider'));
+        o = s.taboption('rule', form.Flag, 'rule_provider', _('Append Rule Provider'));
         o.rmempty = false;
 
         o = s.taboption('rule', form.SectionValue, '_rule_providers', form.GridSection, 'rule_provider', _('Edit Rule Providers'));
@@ -405,7 +416,7 @@ return view.extend({
         so.modalonly = true;
         so.depends('type', 'http');
 
-        o = s.taboption('rule', form.Flag, 'rule', '*' + ' ' + _('Append Rule'));
+        o = s.taboption('rule', form.Flag, 'rule', _('Append Rule'));
         o.rmempty = false;
 
         o = s.taboption('rule', form.SectionValue, '_rules', form.TableSection, 'rule', _('Edit Rules'));
@@ -421,8 +432,7 @@ return view.extend({
         so.rmempty = false;
 
         so = o.subsection.option(form.Value, 'type', _('Type'));
-        so.optional = true;
-        so.rmempty = true;
+        so.rmempty = false;
         so.value('RULE-SET', _('Rule Set'));
         so.value('DOMAIN', _('Domain Name'));
         so.value('DOMAIN-SUFFIX', _('Domain Name Suffix'));
@@ -453,37 +463,34 @@ return view.extend({
         s.tab('geox', _('GeoX Config'));
 
         o = s.taboption('geox', form.ListValue, 'geoip_format', _('GeoIP Format'));
+        o.optional = true;
         o.value('dat', 'DAT');
         o.value('mmdb', 'MMDB');
 
         o = s.taboption('geox', form.ListValue, 'geodata_loader', _('GeoData Loader'));
+        o.optional = true;
         o.value('standard', _('Standard Loader'));
         o.value('memconservative', _('Memory Conservative Loader'));
 
         o = s.taboption('geox', form.Value, 'geosite_url', _('GeoSite Url'));
-        o.rmempty = false;
 
         o = s.taboption('geox', form.Value, 'geoip_mmdb_url', _('GeoIP(MMDB) Url'));
-        o.rmempty = false;
 
         o = s.taboption('geox', form.Value, 'geoip_dat_url', _('GeoIP(DAT) Url'));
-        o.rmempty = false;
 
         o = s.taboption('geox', form.Value, 'geoip_asn_url', _('GeoIP(ASN) Url'));
-        o.rmempty = false;
 
-        o = s.taboption('geox', form.Flag, 'geox_auto_update', _('GeoX Auto Update'));
-        o.rmempty = false;
+        o = s.taboption('geox', form.ListValue, 'geox_auto_update', _('GeoX Auto Update'));
+        o.optional = true;
+        o.value('0', _('Disable'));
+        o.value('1', _('Enable'));
 
         o = s.taboption('geox', form.Value, 'geox_update_interval', _('GeoX Update Interval'));
         o.datatype = 'uinteger';
-        o.placeholder = '24';
-        o.retain = true;
-        o.depends('geox_auto_update', '1');
 
         s.tab('mixin_file_content', _('Mixin File Content'));
 
-        o = s.taboption('mixin_file_content', form.Flag, 'mixin_file_content', '*' + ' ' + _('Enable'), _('Please go to the editor tab to edit the file for mixin'));
+        o = s.taboption('mixin_file_content', form.Flag, 'mixin_file_content', _('Enable'), _('Please go to the editor tab to edit the file for mixin'));
         o.rmempty = false;
 
         return m.render();
--- luci-theme-kucat/Makefile
+++ luci-theme-kucat/Makefile
@@ -10,8 +10,8 @@ THEME_TITLE:=Kucat Theme
 PKG_NAME:=luci-theme-$(THEME_NAME)
 LUCI_TITLE:=Kucat Theme by sirpdboy
 LUCI_DEPENDS:=
-PKG_VERSION:=2.5.5
-PKG_RELEASE:=20250301
+PKG_VERSION:=2.5.6
+PKG_RELEASE:=20250304
 
 define Package/luci-theme-$(THEME_NAME)/postinst
 #!/bin/sh
