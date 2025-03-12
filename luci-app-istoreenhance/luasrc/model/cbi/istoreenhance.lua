b9331322bf64b6eb6df90a26c376dffa3318a0c6
kenzok8 <kenzok8@users.noreply.github.com>
2025-03-12 16:28:40 +0800

    update 2025-03-12 16:28:40

--- luci-app-istoreenhance/luasrc/model/cbi/istoreenhance.lua
+++ luci-app-istoreenhance/luasrc/model/cbi/istoreenhance.lua
@@ -1,5 +1,7 @@
 local m, s
 
+local istoreenhance_model = require "luci.model.istoreenhance"
+
 m = Map("istoreenhance", translate("iStoreEnhance"), translate("iStoreEnhance is a tool to fix network issues for iStore."))
 
 m:section(SimpleSection).template  = "istoreenhance_status"
@@ -10,7 +12,22 @@ s.anonymous=true
 
 s:option(Flag, "enabled", translate("Enable")).rmempty=false
 
+s:option(Value, "adminport", translate("Admin Port")).rmempty=false
+
 s:option(Value, "port", translate("Port")).rmempty=false
+
+o = s:option(Value, "cache", translate("Cache Path").."<b>*</b>")
+o.rmempty = false
+o.datatype = "string"
+
+local blocks = istoreenhance_model.blocks()
+local home = istoreenhance_model.home()
+
+local paths, default_path = istoreenhance_model.find_paths(blocks, home, "Configs")
+for _, val in pairs(paths) do
+  o:value(val, val)
+end
+o.default = default_path
 
 return m
 
