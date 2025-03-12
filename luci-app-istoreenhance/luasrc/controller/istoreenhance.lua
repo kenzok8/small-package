4770045319d6c27551c6b9d8ebc6b8e859cb1991
kenzok8 <kenzok8@users.noreply.github.com>
2025-03-12 16:28:40 +0800

    update 2025-03-12 16:28:40

--- luci-app-istoreenhance/luasrc/controller/istoreenhance.lua
+++ luci-app-istoreenhance/luasrc/controller/istoreenhance.lua
@@ -13,11 +13,11 @@ end
 function istoreenhance_status()
 	local sys  = require "luci.sys"
 	local uci  = require "luci.model.uci".cursor()
-	local port = tonumber(uci:get_first("istoreenhance", "istoreenhance", "port"))
+	local port = tonumber(uci:get_first("istoreenhance", "istoreenhance", "adminport"))
 
 	local status = {
 		running = (sys.call("pidof iStoreEnhance >/dev/null") == 0),
-		port = (port or 8897)
+		port = (port or 5003)
 	}
 
 	luci.http.prepare_content("application/json")
