029920a37c198e9059cfdbaed420726d53f429bf
kenzok8 <kenzok8@users.noreply.github.com>
2025-03-19 16:29:18 +0800

    update 2025-03-19 16:29:18

--- luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
+++ luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
@@ -3,6 +3,39 @@
 ACTION=${1}
 shift 1
 
+
+# 1. 判断是否安装 iStoreEnhance
+# 2. docker info获取Registry Mirrors:下的第一个服务器地址，并且地址包含registry.linkease.net
+# 3. 如果1和2都满足，则直接 docker pull registry.linkease.net:5443/onething1/wxedge
+# 4. 反之运行docker pull
+istoreenhance_pull() {
+  local image_name="$1"
+
+  # 判断是否安装 iStoreEnhance
+  if command -v istoreenhance &> /dev/null; then
+    # 使用 docker info 获取镜像服务器列表
+    local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {getline; print $1}' | tr -d ' ' | awk -F',' '{print $1}')
+    if [[ -n "$registry_mirror" && "$registry_mirror" == *"registry.linkease.net"* ]]; then
+      # 提取主机和端口部分
+      local registry_host=$(echo "$registry_mirror" | sed -E 's|^https?://([^/]+).*|\1|')
+      # 拼接完整的镜像地址
+      local full_image_name="$registry_host/$image_name"
+      # 直接拉取镜像
+      docker pull "$full_image_name"
+      if [ $? -eq 0 ]; then
+        return 0
+      fi
+    fi
+  fi
+
+  # 否则运行 docker pull
+  docker pull "$image_name"
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+  return 0
+}
+
 do_install() {
   local path=`uci get wxedge.@wxedge[0].cache_path 2>/dev/null`
   local image_name=`uci get wxedge.@wxedge[0].image_name 2>/dev/null`
@@ -14,9 +47,8 @@ do_install() {
 
   [ -z "$image_name" ] && image_name="onething1/wxedge"
   echo "docker pull ${image_name}"
-  docker pull ${image_name}
-  RET=$?
-  if [ ! "$RET" = "0" ]; then
+  istoreenhance_pull "$image_name"
+  if [ $? -ne 0 ]; then
     echo "download failed, install istoreenhance to speedup, \"https://doc.linkease.com/zh/guide/istore/software/istoreenhance.html\""
     exit 1
   fi
