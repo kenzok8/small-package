6bc7cb9e81d523d9f22ceb83c4e5a2094ab03f47
kenzok8 <kenzok8@users.noreply.github.com>
2025-03-22 00:26:16 +0800

    update 2025-03-22 00:26:16

--- luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
+++ luci-app-wxedge/root/usr/libexec/istorec/wxedge.sh
@@ -10,47 +10,29 @@ shift 1
 # 4. 反之运行docker pull
 istoreenhance_pull() {
   local image_name="$1"
-  local isInstall=$(command -v iStoreEnhance)
-  local isRun=$(pgrep iStoreEnhance)
+  echo "docker pull ${image_name}"
+  docker pull "$image_name"
+  if [ $? -ne 0 ]; then
+    local isInstall=$(command -v iStoreEnhance)
+    local isRun=$(pgrep iStoreEnhance)
+      # 判断iStoreEnhance是否运行
+    if [ -n "$isRun" ]; then
+      # 使用 docker info 获取包含 registry.linkease.net 的镜像服务器地址
+      local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {found=1; next} found && NF {if ($0 ~ /registry.linkease.net/) {print; exit}}')
 
-  # 判断iStoreEnhance是否运行
-  if [ -n "$isRun" ]; then
-    # 使用 docker info 获取包含 registry.linkease.net 的镜像服务器地址
-    local registry_mirror=$(docker info 2>/dev/null | awk -F': ' '/Registry Mirrors:/ {found=1; next} found && NF {if ($0 ~ /registry.linkease.net/) {print; exit}}')
-
-    if [[ -n "$registry_mirror" ]]; then
-      # 提取主机和端口部分
-      local registry_host=$(echo ${registry_mirror} | sed -E 's|^https?://([^/]+).*|\1|')
-      # 拼接完整的镜像地址
-      local full_image_name="$registry_host/$image_name"
-      echo "istoreenhance_pull ${full_image_name}"
-      # 直接拉取镜像
-      docker pull "$full_image_name"
-      if [ $? -ne 0 ]; then
+      if [[ -n "$registry_mirror" ]]; then
         echo "istoreenhance_pull failed"
-      exit 1
+      else
+        echo "download failed, not found registry.linkease.net"
       fi
     else
-      echo "istoreenhance_pull ${image_name}"
-      docker pull "$image_name"
-      if [ $? -ne 0 ]; then
-        echo "download failed, not found registry.linkease.net"
-      exit 1
-      fi
-    fi
-  else
-    # 否则运行 docker pull
-    echo "docker pull ${image_name}"
-    docker pull "$image_name"
-    if [ $? -ne 0 ]; then
-    # 判断是否安装 iStoreEnhance
       if [ -z "$isInstall" ]; then
         echo "download failed, install istoreenhance to speedup, \"https://doc.linkease.com/zh/guide/istore/software/istoreenhance.html\""
       else
         echo "download failed, enable istoreenhance to speedup"
       fi
-      exit 1
     fi
+    exit 1
   fi
 }
 
