6fc6ad97d45f69176287b2dbbbe1ff9d07006aab
kenzok8 <kenzok8@users.noreply.github.com>
2025-04-16 20:42:33 +0800

    update 2025-04-16 20:42:33

--- nikki/files/scripts/debug.sh
+++ nikki/files/scripts/debug.sh
@@ -41,12 +41,12 @@ fi
 \`\`\`json
 `
 ucode -S -e '
-import { connect } from "ubus";
-const ubus = connect();
-const config = ubus.call("uci", "get", { "config": "nikki" });
+import { cursor } from "uci";
+const uci = cursor();
+const config = uci.get_all("nikki");
 const result = {};
-for (let section_id in config.values) {
-    const section = config.values[section_id];
+for (let section_id in config) {
+    const section = config[section_id];
     const section_type = section[".type"];
 	if (result[section_type] == null) {
 		result[section_type] = [];
