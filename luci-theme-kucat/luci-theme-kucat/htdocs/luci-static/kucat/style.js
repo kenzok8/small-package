1a0aa470db35f595ada41e497e0b7d4dc3a77053
kenzok8 <kenzok8@users.noreply.github.com>
2025-04-21 04:22:18 +0800

    update 2025-04-21 04:22:18

--- luci-theme-kucat/luci-theme-kucat/htdocs/luci-static/kucat/style.js
+++ luci-theme-kucat/luci-theme-kucat/htdocs/luci-static/kucat/style.js
@@ -7,19 +7,18 @@
  *
  *  Licensed to the public under the Apache License 2.0
  */
-
 function pdopenbar() {
     document.getElementById("header-bar-left").style.width = "300px";
     document.getElementById("header-bar-left").style.display = "block";
     document.getElementById("header-bar-right").style.width = "0";
-    document.getElementById("header-bar-right").style.display = "none"
+    document.getElementById("header-bar-right").style.display = "none";
 }
 
 function pdclosebar() {
     document.getElementById("header-bar-left").style.display = "none";
     document.getElementById("header-bar-left").style.width = "0";
     document.getElementById("header-bar-right").style.display = "block";
-    document.getElementById("header-bar-right").style.width = "50px"
+    document.getElementById("header-bar-right").style.width = "50px";
 }
 
 function initScrollContainers() {
