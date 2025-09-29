f72e7d5d7a787aacb6b79fd6a9866a8941cef186
kenzok8 <kenzok8@users.noreply.github.com>
2025-09-30 00:22:52 +0800

    update 2025-09-30 00:22:52

--- luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
+++ luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
@@ -144,7 +144,7 @@ return view.extend({
 
 		so = ss.taboption('field_general', form.Value, 'label', _('Label'));
 		so.load = hm.loadDefaultLabel;
-		so.validate = function(/* ... */) { return hm.validateUniqueValue.apply(this, arguments) }
+		so.validate = hm.validateUniqueValue;
 		so.modalonly = true;
 
 		so = ss.taboption('field_general', form.Flag, 'enabled', _('Enable'));
@@ -485,18 +485,14 @@ return view.extend({
 		so = ss.taboption('field_general', form.Value, 'wireguard_private_key', _('Private key'),
 			_('WireGuard requires base64-encoded private keys.'));
 		so.password = true;
-		so.validate = function(/* ... */) {
-			return hm.validateBase64Key.call(this, 44, ...arguments);
-		}
+		so.validate = L.bind(hm.validateBase64Key, so, 44);
 		so.rmempty = false;
 		so.depends('type', 'wireguard');
 		so.modalonly = true;
 
 		so = ss.taboption('field_general', form.Value, 'wireguard_peer_public_key', _('Peer pubkic key'),
 			_('WireGuard peer public key.'));
-		so.validate = function(/* ... */) {
-			return hm.validateBase64Key.call(this, 44, ...arguments);
-		}
+		so.validate = L.bind(hm.validateBase64Key, so, 44);
 		so.rmempty = false;
 		so.depends('type', 'wireguard');
 		so.modalonly = true;
@@ -504,9 +500,7 @@ return view.extend({
 		so = ss.taboption('field_general', form.Value, 'wireguard_pre_shared_key', _('Pre-shared key'),
 			_('WireGuard pre-shared key.'));
 		so.password = true;
-		so.validate = function(/* ... */) {
-			return hm.validateBase64Key.call(this, 44, ...arguments);
-		}
+		so.validate = L.bind(hm.validateBase64Key, so, 44);
 		so.depends('type', 'wireguard');
 		so.modalonly = true;
 
@@ -1169,7 +1163,7 @@ return view.extend({
 		/* General fields */
 		so = ss.taboption('field_general', form.Value, 'label', _('Label'));
 		so.load = hm.loadDefaultLabel;
-		so.validate = function(/* ... */) { return hm.validateUniqueValue.apply(this, arguments) }
+		so.validate = hm.validateUniqueValue;
 		so.modalonly = true;
 
 		so = ss.taboption('field_general', form.Flag, 'enabled', _('Enable'));
@@ -1442,7 +1436,7 @@ return view.extend({
 
 		so = ss.option(form.Value, 'label', _('Label'));
 		so.load = hm.loadDefaultLabel;
-		so.validate = function(/* ... */) { return hm.validateUniqueValue.apply(this, arguments) }
+		so.validate = hm.validateUniqueValue;
 		so.modalonly = true;
 
 		so = ss.option(form.Flag, 'enabled', _('Enable'));
