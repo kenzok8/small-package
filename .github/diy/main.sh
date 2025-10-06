a3eb7c5cdbb9f20fdabdeaf1f8630027037785c0
有種 <39034242+kenzok8@users.noreply.github.com>
2025-10-06 16:43:32 +0800

    Add git clone commands for additional repositories

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -16,6 +16,7 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
+(
 #git clone --depth 1 https://github.com/kenzo78/my-packages && mvdir my-packages
 git clone --depth 1 https://github.com/kiddin9/luci-app-dnsfilter
 git clone --depth 1 https://github.com/kiddin9/aria2
