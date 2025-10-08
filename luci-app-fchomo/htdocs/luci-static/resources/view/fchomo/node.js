1c409f0aab457303a6b367d5b8ea2ddbf14ef4f0
kenzok8 <kenzok8@users.noreply.github.com>
2025-10-08 14:19:03 +0800

    update 2025-10-08 14:19:03

--- luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
+++ luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
@@ -5,7 +5,88 @@
 'require view';
 
 'require fchomo as hm';
+'require firewall as fw';
 'require tools.widgets as widgets';
+
+document.querySelector('head').appendChild(E('link', {
+	'rel': 'stylesheet',
+	'type': 'text/css',
+	'href': L.resource('view/fchomo/node.css')
+}));
+
+const CBIBubblesValue = form.DummyValue.extend({
+	__name__: 'CBI.BubblesValue',
+
+	load(section_id) {
+		const uciconfig = this.config || this.section.configthis.config || this.map.config;
+		const type = uci.get(uciconfig, section_id, 'type');
+		const detour = uci.get(uciconfig, section_id, 'chain_tail_group') || uci.get(uciconfig, section_id, 'chain_tail');
+
+		switch (type) {
+			case 'node':
+				return '%s ⇒ %s'.format(
+					uci.get(uciconfig, section_id, 'chain_head'),
+					detour
+				);
+			case 'provider':
+				return '%s ⇒ %s'.format(
+					uci.get(uciconfig, section_id, 'chain_head_sub'),
+					detour
+				);
+			default:
+				return null;
+		}
+	},
+
+	textvalue(section_id) {
+		const cval = this.cfgvalue(section_id);
+		if (cval === null)
+			return null;
+
+		const chain = cval.split('⇒').map(t => t.trim());
+		//const container_id = this.cbid(section_id) + '.bubbles';
+
+		let curWrapper = null;
+		for (let i = 0; i < chain.length; i++) {
+			const value = chain[i];
+
+			const labelEl = E('span', {
+				class: 'bubble-label'
+			}, [ value ]);
+
+			const bubbleEl = E('div', {
+				class: 'bubble',
+				//id: container_id + `.${hm.toUciname(value)}`,
+				style: fw.getZoneColorStyle(value)
+			}, [ labelEl ]);
+
+			if (curWrapper)
+				bubbleEl.insertBefore(curWrapper, bubbleEl.firstChild);
+
+			curWrapper = bubbleEl;
+		}
+
+		return E('div', {
+			class: 'nested-bubbles-container',
+			//id: container_id
+		}, [ curWrapper ]);
+	},
+
+	hexToRgbArray(hex) {
+		// Remove the '#' if it exists
+		const shorthandRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+		const result = shorthandRegex.exec(hex);
+
+		if (result)
+			return [
+				parseInt(result[1], 16),
+				parseInt(result[2], 16),
+				parseInt(result[3], 16)
+			];
+		else
+			return null;
+	}
+});
 
 function parseProviderYaml(field, name, cfg) {
 	if (!cfg.type)
@@ -1449,26 +1530,7 @@ return view.extend({
 		so.default = 'node';
 		so.textvalue = hm.textvalue2Value;
 
-		so = ss.option(form.DummyValue, '_value', _('Value'));
-		so.load = function(section_id) {
-			const type = uci.get(data[0], section_id, 'type');
-			const detour = uci.get(data[0], section_id, 'chain_tail_group') || uci.get(data[0], section_id, 'chain_tail');
-
-			switch (type) {
-				case 'node':
-					return '%s » %s'.format(
-						uci.get(data[0], section_id, 'chain_head'),
-						detour
-					);
-				case 'provider':
-					return '%s » %s'.format(
-						uci.get(data[0], section_id, 'chain_head_sub'),
-						detour
-					);
-				default:
-					return null;
-			}
-		}
+		so = ss.option(CBIBubblesValue, '_value', _('Value'));
 		so.modalonly = false;
 
 		so = ss.option(form.ListValue, 'chain_head_sub', _('Chain head') + _(' (Destination)'));
