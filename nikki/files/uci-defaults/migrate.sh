dcdedaefbffd4620808e45a67eb3894691e64144
kenzok8 <kenzok8@users.noreply.github.com>
2025-10-15 20:48:29 +0800

    update 2025-10-15 20:48:28

--- nikki/files/uci-defaults/migrate.sh
+++ nikki/files/uci-defaults/migrate.sh
@@ -190,6 +190,15 @@ proxy_reserved_ip6=$(uci -q get nikki.proxy.reserved_ip6); [ -z "$proxy_reserved
 	uci add_list nikki.proxy.reserved_ip6=ff00::/8
 }
 
+# since v1.24.3
+
+proxy_bypass_china_mainland_ip=$(uci -q get nikki.proxy.bypass_china_mainland_ip)
+proxy_bypass_china_mainland_ip6=$(uci -q get nikki.proxy.bypass_china_mainland_ip6)
+[ -z "$proxy_bypass_china_mainland_ip6" ] && uci set nikki.proxy.bypass_china_mainland_ip6=$proxy_bypass_china_mainland_ip
+
+routing_tproxy_fw_mask=$(uci -q get nikki.routing.tproxy_fw_mask); [ -z "$routing_tproxy_fw_mask" ] && uci set nikki.routing.tproxy_fw_mask=0xFF
+routing_tun_fw_mask=$(uci -q get nikki.routing.tun_fw_mask); [ -z "$routing_tun_fw_mask" ] && uci set nikki.routing.tun_fw_mask=0xFF
+
 # commit
 uci commit nikki
 
