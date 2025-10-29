d6d2013584e65cf997ef43f55441e3437ea240ca
kenzok8 <kenzok8@users.noreply.github.com>
2025-10-29 20:48:35 +0800

    update 2025-10-29 20:48:35

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
@@ -29,20 +29,20 @@ mydevice.default = "PLATFORM: " .. mydevice_platfrom
 mydevice.rmempty = false
 
 --2.Set OpenWrt Firmware Repository
-firmware_repo = o:option(Value, "amlogic_firmware_repo", translate("Download repository of OpenWrt:"))
-firmware_repo.description = translate("Set the download repository of the OpenWrt files on github.com in [Online Download Update].")
+firmware_repo = o:option(Value, "amlogic_firmware_repo", translate("OpenWrt download repository:"))
+firmware_repo.description = translate("Set the OpenWrt files download repository on github.com in [Online Download Update].")
 firmware_repo.default = "https://github.com/breakingbadboy/OpenWrt"
 firmware_repo.rmempty = false
 
 --3.Set OpenWrt Releases's Tag Keywords
-firmware_tag = o:option(Value, "amlogic_firmware_tag", translate("Keywords of Tags in Releases:"))
-firmware_tag.description = translate("Set the keywords of Tags in Releases of github.com in [Online Download Update].")
+firmware_tag = o:option(Value, "amlogic_firmware_tag", translate("OpenWrt download tags keyword:"))
+firmware_tag.description = translate("Set the OpenWrt files download tags keyword for github.com in [Online Download Update].")
 firmware_tag.default = "ARMv8"
 firmware_tag.rmempty = false
 
 --4.Set OpenWrt Firmware Suffix
-firmware_suffix = o:option(Value, "amlogic_firmware_suffix", translate("Suffix of OpenWrt files:"))
-firmware_suffix.description = translate("Set the suffix of the OpenWrt in Releases of github.com in [Online Download Update].")
+firmware_suffix = o:option(Value, "amlogic_firmware_suffix", translate("OpenWrt files suffix:"))
+firmware_suffix.description = translate("Set the OpenWrt files download suffix for github.com in [Online Download Update].")
 firmware_suffix:value(".7z", translate(".7z"))
 firmware_suffix:value(".zip", translate(".zip"))
 firmware_suffix:value(".img.gz", translate(".img.gz"))
@@ -51,14 +51,14 @@ firmware_suffix.default = ".img.gz"
 firmware_suffix.rmempty = false
 
 --5.Set OpenWrt Kernel DownLoad Path
-kernel_path = o:option(Value, "amlogic_kernel_path", translate("Download path of OpenWrt kernel:"))
-kernel_path.description = translate("Set the download path of the kernel in the github.com repository in [Online Download Update].")
+kernel_path = o:option(Value, "amlogic_kernel_path", translate("Kernel download repository:"))
+kernel_path.description = translate("Set the kernel files download repository on github.com in [Online Download Update].")
 kernel_path:value("https://github.com/breakingbadboy/OpenWrt")
 kernel_path:value("https://github.com/ophub/kernel")
 kernel_path.default = "https://github.com/breakingbadboy/OpenWrt"
 kernel_path.rmempty = false
 
--- 6. Set OpenWrt Kernel Tags
+--6. Set OpenWrt Kernel Tags
 -- Read the currently SAVED value of the kernel path.
 local current_kernel_path = trim(luci.sys.exec("uci get amlogic.@amlogic[0].amlogic_kernel_path 2>/dev/null") or "")
 -- If it's not set yet, use its default value for the logic below.
@@ -104,8 +104,8 @@ else
     end
 end
 -- Create the kernel tags option.
-kernel_tags = o:option(Value, "amlogic_kernel_tags", translate("Download tags of OpenWrt kernel:"))
-kernel_tags.description = translate("Set the download tags of the kernel in the github.com repository in [Online Download Update].")
+kernel_tags = o:option(Value, "amlogic_kernel_tags", translate("Kernel download tags:"))
+kernel_tags.description = translate("Set the kernel files download tags on github.com in [Online Download Update].")
 -- Populate the dropdown with the known tags.
 for value, display_name in pairs(known_tags) do
     kernel_tags:value(value, translate(display_name))
@@ -118,9 +118,9 @@ end
 kernel_tags.default = kernel_tagsname
 kernel_tags.rmempty = false
 
--- 7.Set kernel version branch
+--7.Set kernel version branch
 kernel_branch = o:option(Value, "amlogic_kernel_branch", translate("Set version branch:"))
-kernel_branch.description = translate("Set the version branch of the openwrt firmware and kernel selected in [Online Download Update].")
+kernel_branch.description = translate("Set the version branch of the OpenWrt files and kernel selected in [Online Download Update].")
 kernel_branch:value("5.4", translate("5.4"))
 kernel_branch:value("5.10", translate("5.10"))
 kernel_branch:value("5.15", translate("5.15"))
