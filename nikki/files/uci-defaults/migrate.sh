5d0f35c8417598cc531aaef7f618ae0a383002ef
kenzok8 <kenzok8@users.noreply.github.com>
2025-11-09 16:25:42 +0800

    update 2025-11-09 16:25:42

--- nikki/files/uci-defaults/migrate.sh
+++ nikki/files/uci-defaults/migrate.sh
@@ -140,12 +140,6 @@ uci show nikki | grep -o -E 'nikki\.@router_access_control\[[[:digit:]]+\]=route
 	done
 done
 
-# since v1.23.2
-
-env_disable_safe_path_check=$(uci -q get nikki.env.disable_safe_path_check); [ -n "$env_disable_safe_path_check" ] && uci del nikki.env.disable_safe_path_check
-
-env_skip_system_ipv6_check=$(uci -q get nikki.env.skip_system_ipv6_check); [ -z "$env_skip_system_ipv6_check" ] && uci set nikki.env.skip_system_ipv6_check=0
-
 # since v1.23.3
 
 uci show nikki | grep -o -E 'nikki\.@router_access_control\[[[:digit:]]+\]=router_access_control' | cut -d '=' -f 1 | while read -r router_access_control; do
