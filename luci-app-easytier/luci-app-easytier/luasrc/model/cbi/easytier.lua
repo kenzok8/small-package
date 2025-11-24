c5cd3c4a1dc484c2687077c9cae5a9021d9c77e8
kenzok8 <kenzok8@users.noreply.github.com>
2025-11-24 16:34:08 +0800

    update 2025-11-24 16:34:08

--- luci-app-easytier/luci-app-easytier/luasrc/model/cbi/easytier.lua
+++ luci-app-easytier/luci-app-easytier/luasrc/model/cbi/easytier.lua
@@ -5,9 +5,8 @@ m = Map("easytier")
 m.description = translate("A simple, secure, decentralized VPN solution for intranet penetration, implemented in Rust using the Tokio framework. "
         .. "Project URL: <a href=\"https://github.com/EasyTier/EasyTier\" target=\"_blank\">github.com/EasyTier/EasyTier</a>&nbsp;&nbsp;"
         .. "<a href=\"http://easytier.cn\" target=\"_blank\">Official Documentation</a>&nbsp;&nbsp;"
-        .. "<a href=\"http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jhP2Z4UsEZ8wvfGPLrs0VwLKn_uz0Q_p&authKey=OGKSQLfg61YPCpVQuvx%2BxE7hUKBVBEVi9PljrDKbHlle6xqOXx8sOwPPTncMambK&noverify=0&group_code=949700262\" target=\"_blank\">QQ Group</a>&nbsp;&nbsp;"
-        .. "<a href=\"https://doc.oee.icu\" target=\"_blank\">Beginner Tutorial</a>")
-
+        .. "<a href=\"http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jhP2Z4UsEZ8wvfGPLrs0VwLKn_uz0Q_p&authKey=OGKSQLfg61YPCpVQuvx%2BxE7hUKBVBEVi9PljrDKbHlle6xqOXx8sOwPPTncMambK&noverify=0&group_code=949700262\" target=\"_blank\">QQ Group</a>&nbsp;&nbsp;")
+  
 m:section(SimpleSection).template  = "easytier/easytier_status"
 
 -- easytier-core
