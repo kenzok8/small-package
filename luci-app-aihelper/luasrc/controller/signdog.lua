54802a96bc0a6221678a9cbb811027a431eebf28
kenzok8 <kenzok8@users.noreply.github.com>
2025-11-26 20:50:28 +0800

    update 2025-11-26 20:50:27

---
+++ luci-app-aihelper/luasrc/controller/signdog.lua
@@ -0,0 +1,25 @@
+local http = require "luci.http"
+
+module("luci.controller.aihelper", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/aihelper") then
+		return
+	end
+	local page
+	page = entry({"admin", "services","aihelper"}, cbi("aihelper"), _("aihelper"), 100)
+	page.dependent = true
+    entry({"admin", "services", "aihelper_status"}, call("aihelper_status"))
+
+end
+
+function aihelper_status()
+	local sys  = require "luci.sys"
+	local uci  = require "luci.model.uci".cursor()
+	local status = {
+		running = (sys.call("pidof aihelper >/dev/null") == 0),
+		port = 9930
+	}
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(status)
+end
