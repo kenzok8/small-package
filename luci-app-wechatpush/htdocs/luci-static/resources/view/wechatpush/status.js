cd2bad8b9a89e147f356ff30d89f78fb902cb5b4
kenzok8 <kenzok8@users.noreply.github.com>
2025-12-20 04:28:27 +0800

    update 2025-12-20 04:28:27

--- luci-app-wechatpush/htdocs/luci-static/resources/view/wechatpush/status.js
+++ luci-app-wechatpush/htdocs/luci-static/resources/view/wechatpush/status.js
@@ -1,6 +1,7 @@
 'use strict';
 'require view';
 'require rpc';
+'require uci';
 
 var callServiceList = rpc.declare({
     object: 'service',
@@ -22,15 +23,22 @@ function getServiceStatus() {
 
 return view.extend({
     load: function () {
-        return getServiceStatus();
+        return Promise.all([
+            getServiceStatus(),
+            uci.load('wechatpush')
+        ]);
     },
 
-    render: function (status) {
-        // 如果服务正在运行，跳转到 client 页面
-        if (status) {
-            window.location.pathname = '/cgi-bin/luci/admin/services/wechatpush/client';
+    render: function (data) {
+        var status = data[0];
+        // 检查配置，默认为 '1' (开启)
+        var preferClient = uci.get('wechatpush', 'config', 'prefer_client_page') !== '0';
+
+        // 如果服务正在运行 且 优先展示在线设备开关开启，跳转到 client 页面
+        if (status && preferClient) {
+            window.location.replace('/cgi-bin/luci/admin/services/wechatpush/client');
         } else {
-            window.location.pathname = '/cgi-bin/luci/admin/services/wechatpush/config';
+            window.location.replace('/cgi-bin/luci/admin/services/wechatpush/config');
         }
     },
 
