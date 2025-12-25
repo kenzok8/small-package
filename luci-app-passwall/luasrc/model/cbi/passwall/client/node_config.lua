afe4964327bbb8055625c8c0854e9b31f3c0f486
kenzok8 <kenzok8@users.noreply.github.com>
2025-12-25 20:48:35 +0800

    update 2025-12-25 20:48:35

--- luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
@@ -40,7 +40,7 @@ for k, v in pairs(groups) do
 	o:value(k)
 end
 o.write = function(self, section, value)
-	value = api.trim(value)
+	value = value and value:gsub("%s+", "") or ""
 	local lower = value:lower()
 
 	if lower == "" or lower == "default" then
