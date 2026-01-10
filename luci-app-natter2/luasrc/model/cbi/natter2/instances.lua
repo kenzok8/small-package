0ac0e654eafe414dd0d5169addaee363f6af92de
kenzok8 <kenzok8@users.noreply.github.com>
2026-01-10 20:46:27 +0800

    update 2026-01-10 20:46:27

--- luci-app-natter2/luasrc/model/cbi/natter2/instances.lua
+++ luci-app-natter2/luasrc/model/cbi/natter2/instances.lua
@@ -1,8 +1,8 @@
 m = Map("natter2", translate("Instances Settings"),
-translate("")
-.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/usage.md">]]
-.. translate("Instructions")
-.. [[</a>]])
+	translate("")
+	.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/usage.md">]]
+	.. translate("Instructions")
+	.. [[</a>]])
 m.redirect = luci.dispatcher.build_url("admin", "network", "natter2")
 
 s = m:section(NamedSection, arg[1], "instances", "")
@@ -40,31 +40,33 @@ keepalive_server:depends({enable_keepalive_server = "1"})
 interval = s:option(Value, "interval", translate("Interval (Seconds)"), translate("The number of seconds between keepalive"))
 interval.default = 15
 interval.datatype = "uinteger"
+enable_upnp_service = s:option(Flag, "enable_upnp_service", translate("Enable UPnP Service"),
+	translate("Using UPnP to map ports on your device"))
 
-enable_bonding = s:option(Flag, "enable_bonding", translate("Enable Bonding Options"), translate("Usually there is no need to enable binding"))
-enable_bonding.rmempty = true
-bonding_interface = s:option(Value, "bonding_interface", translate("Bonding Interface"))
-bonding_interface.rmempty = true
-bonding_interface.default = '0.0.0.0'
-bonding_interface:depends({enable_bonding = "1"})
+enable_binding = s:option(Flag, "enable_binding", translate("Enable Binding Options"), translate("Usually there is no need to enable binding"))
+enable_binding.rmempty = true
+binding_interface = s:option(Value, "binding_interface", translate("Binding Interface"))
+binding_interface.rmempty = true
+binding_interface.default = '0.0.0.0'
+binding_interface:depends({enable_binding = "1"})
 
-bonding_port = s:option(Value, "bonding_port", translate("Bonding Port"))
-bonding_port.rmempty = true
-bonding_port.default = '0'
-bonding_port:depends({enable_bonding = "1"})
+binding_port = s:option(Value, "binding_port", translate("Binding Port"))
+binding_port.rmempty = true
+binding_port.default = '0'
+binding_port:depends({enable_binding = "1"})
 
 enable_forwarding = s:option(Flag, "enable_forwarding", translate("Enable Forwarding Options"), translate("Forwarding to internal devices"))
 
 forwarding_method = s:option(ListValue, "forwarding_method", translate("Forwarding Method"),
-translate("")
-.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/forward.md">]]
-.. translate("Instructions for forwarding method")
-.. [[</a>]])
+	translate("")
+	.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/forward.md">]]
+	.. translate("Instructions for forwarding method")
+	.. [[</a>]])
 forwarding_method:value('socket', translate("socket (Not Recommended)"))
 if check_binary("iptables") then
 	forwarding_method:value('iptables', translate("iptables (Recommended)"))
 end
-if check_binary("nftables") then
+if check_binary("nft") then
 	forwarding_method:value('nftables', translate("nftables (Recommended)"))
 end
 if check_binary("socat") then
@@ -79,9 +81,9 @@ forwarding_method:depends({enable_forwarding = "1"})
 target_address = s:option(Value, "target_address", translate("Target Address"))
 target_address.datatype = "ipmask4"
 luci.sys.net.ipv4_hints(
-function(ip, name)
+	function(ip, name)
 	target_address:value(ip, "%s (%s)" %{ ip, name })
-end)
+	end)
 target_address:depends({enable_forwarding = "1"})
 
 target_port = s:option(Value, "target_port", translate("Target Port"))
@@ -108,10 +110,10 @@ log_level:value('verbose', translate("Verbose"))
 enable_notify = s:option(Flag,"enable_notify", translate("Enable Notify Script"))
 enable_notify.rmempty = false
 notify_path = s:option(Value, "notify_path", translate("Notify Script Path"),
-translate("")
-.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/script.md">]]
-.. translate("Instructions for using the notification script")
-.. [[</a>]])
+	translate("")
+	.. [[<a href="https://github.com/MikeWang000000/Natter/blob/master/docs/script.md">]]
+	.. translate("Instructions for using the notification script")
+	.. [[</a>]])
 notify_path.rmempty = true
 notify_path.default = "/usr/share/luci-app-natter2/notify-example.sh"
 notify_path:depends({enable_notify = "1"})
