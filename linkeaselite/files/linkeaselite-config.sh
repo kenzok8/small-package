dbd4da6d20a0ba7889436c7c078593fbc78421cb
kenzok8 <kenzok8@users.noreply.github.com>
2026-07-27 14:54:33 +0800

    update 2026-07-27 14:54:33

--- linkeaselite/files/linkeaselite-config.sh
+++ linkeaselite/files/linkeaselite-config.sh
@@ -4,34 +4,25 @@
 
 case "$1" in
   save)
-    if [ ! -z "$2" ]; then
-      uci set "linkease.@linkease[0].preconfig=$2"
-      uci commit
+    if [ -n "$2" ]; then
+      uci set "linkeaselite.@linkeaselite[0].preconfig=$2"
+      uci commit linkeaselite
     fi
     ;;
 
   load)
-    if [ -f "/usr/sbin/preconfig.data" ]; then
-      data="`cat /usr/sbin/preconfig.data`"
-      uci set "linkease.@linkease[0].preconfig=${data}"
-      uci commit
-      rm /usr/sbin/preconfig.data
-    else
-      data="`uci -q get linkease.@linkease[0].preconfig`"
-    fi
-
+    data="`uci -q get linkeaselite.@linkeaselite[0].preconfig`"
     if [ -z "${data}" ]; then
       echo "nil"
     else
       echo "${data}"
     fi
-
     ;;
 
   local_save)
-    if [ ! -z "$2" ]; then
-      uci set "linkease.@linkease[0].local_home=$2"
-      uci commit
+    if [ -n "$2" ]; then
+      uci set "linkeaselite.@linkeaselite[0].local_home=$2"
+      uci commit linkeaselite
       ROOT_DIR="$2"
       if [ -f "/etc/config/quickstart" ]; then
         config_load quickstart
@@ -40,23 +31,21 @@ case "$1" in
         config_get PUB_DIR main pub_dir ""
         config_get DL_DIR main dl_dir ""
         config_get TMP_DIR main tmp_dir ""
-        if [ "$ROOT_DIR" = "$MAIN_DIR" ]; then
-          exit 0
-        fi
+        [ "$ROOT_DIR" = "$MAIN_DIR" ] && exit 0
         uci set "quickstart.main.main_dir=$ROOT_DIR"
-        if [ -z "$CONF_DIR" -o "$CONF_DIR" = "$MAIN_DIR/Configs" ]; then
+        if [ -z "$CONF_DIR" ] || [ "$CONF_DIR" = "$MAIN_DIR/Configs" ]; then
           uci set "quickstart.main.conf_dir=$ROOT_DIR/Configs"
         fi
-        if [ -z "$PUB_DIR" -o "$PUB_DIR" = "$MAIN_DIR/Public" ]; then
+        if [ -z "$PUB_DIR" ] || [ "$PUB_DIR" = "$MAIN_DIR/Public" ]; then
           uci set "quickstart.main.pub_dir=$ROOT_DIR/Public"
         fi
-        if [ -z "$DL_DIR" -o "$DL_DIR" = "$MAIN_DIR/Public/Downloads" ]; then
+        if [ -z "$DL_DIR" ] || [ "$DL_DIR" = "$MAIN_DIR/Public/Downloads" ]; then
           uci set "quickstart.main.dl_dir=$ROOT_DIR/Public/Downloads"
         fi
-        if [ -z "$TMP_DIR" -o "$TMP_DIR" = "$MAIN_DIR/Caches" ]; then
+        if [ -z "$TMP_DIR" ] || [ "$TMP_DIR" = "$MAIN_DIR/Caches" ]; then
           uci set "quickstart.main.tmp_dir=$ROOT_DIR/Caches"
         fi
-        uci commit
+        uci commit quickstart
       fi
     fi
     ;;
@@ -66,19 +55,21 @@ case "$1" in
       data="`uci -q get quickstart.main.main_dir`"
     fi
     if [ -z "$data" ]; then
-      data="`uci -q get linkease.@linkease[0].local_home`"
+      data="`uci -q get linkeaselite.@linkeaselite[0].local_home`"
     fi
-
     if [ -z "${data}" ]; then
       echo "nil"
     else
       echo "${data}"
     fi
-
     ;;
 
   status)
-    echo "TODO"
+    if pidof linkease-lite >/dev/null 2>&1; then
+      echo "running"
+    else
+      echo "stopped"
+    fi
     ;;
 
   *)
